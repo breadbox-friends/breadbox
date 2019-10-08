0744445677792d62ad1fcf8a68c13f45f0789a9e
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-08 22:06:45 +1100

    Initial signin functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import SignInScreen from './SignInScreen/SignInScreen';
 
 function App() {
-  return (
+  const [userSignedIn, setSignIn] = useState(false);
+
+  return userSignedIn ? (
     <div className="App">
+      <h1>{`Welcome to BreadBox, ${firebase.auth().currentUser.displayName}`}</h1>
       <header className="App-header">
+        <img src={firebase.auth().currentUser.photoURL} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <button onClick={() => {
+          firebase.auth().signOut();
+          setSignIn(false);
+        }
+      }>Click here to sign-out</button>
     </div>
-  );
+  ) : (
+    <SignInScreen
+      setSignInState={ setSignIn }
+    />
+  )
 }
 
 export default App;
