2feb244aec7d18b7a0c456cf33095080edc1292c
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-15 21:49:30 +1100

    Rework some styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Container from 'react-bootstrap/Container';
 
 import MainNav from './MainNav/MainNav';
 import SignInScreen from './SignInScreen/SignInScreen';
-import GroceryItem from './GroceryItem/GroceryItem'
+import GroceryItem from './GroceryItem/GroceryItem';
+import FuzzySearchBar from './FuzzySearchBar/FuzzySearchBar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [userSignedIn, setSignIn] = useState(false);
@@ -19,22 +22,56 @@ function App() {
       title: 'Lemon Water',
       desc: 'Water flavoured lemon!',
       img: 'https://img.icons8.com/color/64/000000/fenix.png'
-    }
+    },
+    {
+      title: 'Apple Watermelon',
+      desc: 'Watermelon flavoured apple!',
+      img: 'https://img.icons8.com/dusk/64/000000/unicorn.png'
+    },
+    {
+      title: 'Lemon Water',
+      desc: 'Water flavoured lemon!',
+      img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    },
+    {
+      title: 'Apple Watermelon',
+      desc: 'Watermelon flavoured apple!',
+      img: 'https://img.icons8.com/dusk/64/000000/unicorn.png'
+    },
+    {
+      title: 'Lemon Water',
+      desc: 'Water flavoured lemon!',
+      img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    },
+    {
+      title: 'Lemon Water',
+      desc: 'Water flavoured lemon!',
+      img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    },
    ]
 
   return userSignedIn ? (
     <React.Fragment>
       <MainNav />
-      <Container className={'body-container'}>
-          {mockFetchItems.map(item => (
-              <GroceryItem
-                itemTitle={item.title}
-                itemDesc={item.desc}
-                imgUrl={item.img}
-                // replace with item.id in the future
-                key={item.title}
-              />
-          ))}
+      <Container className='body-container'>
+        <Row>
+          <Col>
+            <FuzzySearchBar />
+          </Col>
+        </Row>
+        <Row className='grocery-container-row'>
+          <Col className='grocery-container-col'>
+            {mockFetchItems.map(item => (
+                <GroceryItem
+                  itemTitle={item.title}
+                  itemDesc={item.desc}
+                  imgUrl={item.img}
+                  // replace with item.id in the future
+                  key={item.title}
+                />
+            ))}
+          </Col>
+        </Row>
       </Container>
       <button
           onClick={() => {
