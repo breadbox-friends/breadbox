3af513fe12a1283a31c3d9ca67ff7b280ac388dd
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-15 21:49:59 +1100

    Add initial FuzzySearchBar component 🐣

---
+++ src/FuzzySearchBar/FuzzySearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+const FuzzySearchBar = () => {
+  return (
+    <Form inline>
+      <FormControl
+        type="text"
+        placeholder="Search"
+        className="mr-sm-2"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+
+  )
+}
+
+export default FuzzySearchBar;
