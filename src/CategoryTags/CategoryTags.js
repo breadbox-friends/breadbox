c6faa8ba91234e0d0cc85c1a163e320f246d8318
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-20 00:20:26 +1100

    Render some default categories

--- src/CategoryTags/CategoryTags.js
+++ src/CategoryTags/CategoryTags.js
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
-const CategoryTags = () => {
+const CategoryTags = ({ allOptions = ["fish", "beef", "brocolli"]} ) => {
   const [selectedCategories, setCategories] = useState([]);
   const [isSelected, setSelected] = useState(false);
+
+  const renderCategoryButton = category => (
+    <Button key={category}>{category}</Button>
+  )
 
   return (
     <React.Fragment>
       <Button onClick={() => setSelected(!isSelected)}>Categories</Button>
-
       <Modal
         centered
         size="lg"
         show={isSelected}
         onHide={() => setSelected(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>What are you looking for?</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+          </Form>
+          <hr />
+          <div>
+            { allOptions.map(category => renderCategoryButton(category)) }
+          </div>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setSelected(false)}>
             Close
