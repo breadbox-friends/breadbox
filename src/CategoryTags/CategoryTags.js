045ea3d8144ebe26177ce5e3b2c5237432738b9d
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-19 23:52:31 +1100

    Initial Modal with placeholders 🧙

--- src/CategoryTags/CategoryTags.js
+++ src/CategoryTags/CategoryTags.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const CategoryTags = () => {
   const [selectedCategories, setCategories] = useState([]);
   const [isSelected, setSelected] = useState(false);
 
-  console.log("11:35 pm: isSelected", isSelected);
   return (
     <React.Fragment>
       <Button onClick={() => setSelected(!isSelected)}>Categories</Button>
+
+      <Modal
+        centered
+        size="lg"
+        show={isSelected}
+        onHide={() => setSelected(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setSelected(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => setSelected(false)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </React.Fragment>
   )
 }
