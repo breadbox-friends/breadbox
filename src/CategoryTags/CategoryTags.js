81f8d9c16adab2c7c7064c17cbbe0be6a6ec9768
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-19 23:36:50 +1100

    Toggle selected states

--- src/CategoryTags/CategoryTags.js
+++ src/CategoryTags/CategoryTags.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "react-bootstrap/Button"
 
 const CategoryTags = () => {
+  const [selectedCategories, setCategories] = useState([]);
+  const [isSelected, setSelected] = useState(false);
+
+  console.log("11:35 pm: isSelected", isSelected);
   return (
     <React.Fragment>
-      <Button>Categories</Button>
+      <Button onClick={() => setSelected(!isSelected)}>Categories</Button>
     </React.Fragment>
   )
 }
