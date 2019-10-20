e38e8b17d77f924c9a3d78bad2e860d69e321975
Rajesh Durai <rajesh.durai@rea-group.com>
2019-10-20 18:02:43 +1100

    Use rendering images with fallback

--- src/mockPayload.js
+++ src/mockPayload.js
@@ -1,38 +1,48 @@
+import breadbox from "../src/assets/breadbox.png";
+
+const renderWithOfflineFallback = (imgSrcThunkable, fallback) => {
+  if(process.env["REACT_APP_OFFLINE_MODE"]) {
+    return fallback;
+  } else {
+    return (typeof imgSrcThunkable) === "function" ? imgSrcThunkable() : imgSrcThunkable
+  }
+}
+
 const mockFetchedItems = [
   {
     title: 'Apple Watermelon',
-    desc: 'Now with added melon!',
-    img: 'https://img.icons8.com/dusk/64/000000/unicorn.png'
+    desc: 'Watermelon flavoured apple!',
+    img: renderWithOfflineFallback('https://img.icons8.com/dusk/64/000000/unicorn.png', breadbox)
   },
   {
     title: 'Lemon Water',
-    desc: 'Not suitable for flight',
-    img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    desc: 'Water flavoured lemon!',
+    img: renderWithOfflineFallback('https://img.icons8.com/color/64/000000/fenix.png', breadbox)
   },
   {
     title: 'Pear Biscuit',
-    desc: 'Very fragile',
-    img: 'https://img.icons8.com/dusk/64/000000/unicorn.png'
+    desc: 'Watermelon flavoured apple!',
+    img: renderWithOfflineFallback('https://img.icons8.com/dusk/64/000000/unicorn.png', breadbox)
   },
   {
     title: 'Strawberry Pistachio',
-    desc: 'Batteries sold separately',
-    img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    desc: 'Water flavoured lemon!',
+    img: renderWithOfflineFallback('https://img.icons8.com/color/64/000000/fenix.png', breadbox)
   },
   {
     title: 'Cherry Salami',
-    desc: 'Impossible to digest',
-    img: 'https://img.icons8.com/dusk/64/000000/unicorn.png'
+    desc: 'Watermelon flavoured apple!',
+    img: renderWithOfflineFallback('https://img.icons8.com/dusk/64/000000/unicorn.png', breadbox)
   },
   {
     title: 'Grape Cheddar',
-    desc: 'Non-alcoholic',
-    img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    desc: 'Water flavoured lemon!',
+    img: renderWithOfflineFallback('https://img.icons8.com/color/64/000000/fenix.png', breadbox)
   },
   {
     title: 'Peach Chicken',
-    desc: 'Consult your doctor first',
-    img: 'https://img.icons8.com/color/64/000000/fenix.png'
+    desc: 'Water flavoured lemon!',
+    img: renderWithOfflineFallback('https://img.icons8.com/dusk/64/000000/unicorn.png', breadbox)
   },
  ]
 
